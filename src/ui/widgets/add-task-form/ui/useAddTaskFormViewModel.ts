8fb999b91e9a2b7dd87fe8690620750d59bd6c63
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:22:41 +0300

    refactor(AddTaskForm): separate task data managment to hook

--- src/ui/widgets/add-task-form/ui/useAddTaskFormViewModel.ts
+++ src/ui/widgets/add-task-form/ui/useAddTaskFormViewModel.ts
@@ -3,37 +3,66 @@ import { useCallback, useState } from "react"
 import { AddTaskUseCase, NewTaskData } from "application/task"
 
 import { container } from "shared/di"
-import { EventBus } from "shared/event-bus"
+import { BusEvent, EventBus } from "shared/event-bus"
 
 import {
   addTaskFormPresentation,
   AddTaskFormViewModel,
 } from "../model"
 
+export type AddTaskFormViewModelDeps = {
+  addTaskUseCase: AddTaskUseCase
+  eventBus: EventBus
+}
+
 export const useAddTaskFormViewModel = (
-  addTaskUseCase = container.get<AddTaskUseCase>(),
-  eventBus = container.get<EventBus>()
+  deps = {
+    addTaskUseCase: container.get<AddTaskUseCase>(),
+    eventBus: container.get<EventBus>(),
+  }
 ): AddTaskFormViewModel => {
+  const { addTaskUseCase, eventBus } = deps
+
+  const { taskData, resetTaskData, setTitle, setDescription } =
+    useTaskData()
+
+  const changeTitle = async (title: NewTaskData["title"]) => {
+    setTitle(title)
+  }
+
+  const changeDescription = async (
+    description: NewTaskData["description"]
+  ) => setDescription(description)
+
+  const addTask = useCallback(
+    async (data: NewTaskData) => {
+      await addTaskUseCase.add(data)
+      resetTaskData()
+      eventBus.publish(BusEvent.newTaskAdded)
+    },
+    [resetTaskData]
+  )
+
+  const uiState = addTaskFormPresentation(taskData)
+
+  return {
+    uiState,
+    changeTitle,
+    changeDescription,
+    addTask,
+  }
+}
+
+const useTaskData = () => {
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
 
   const taskData: NewTaskData = { title, description }
 
-  const resetFields = useCallback(() => {
+  const resetTaskData = useCallback(() => {
     setTitle("")
     setDescription("")
   }, [setTitle, setDescription])
 
-  return {
-    uiState: addTaskFormPresentation(taskData),
-
-    changeTitle: setTitle,
-    changeDescription: setDescription,
-
-    addTask: async (data: NewTaskData) => {
-      await addTaskUseCase.add(data)
-      resetFields()
-      eventBus.publish("newTaskAdded")
-    },
-  }
+  return { taskData, resetTaskData, setTitle, setDescription }
 }
