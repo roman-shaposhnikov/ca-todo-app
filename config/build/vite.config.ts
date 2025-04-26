61c462c201fb59140ecb0a6fac04bd8935854b2f
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:22:41 +0300

    build(vite): rewrite di plugin with official rollup library

--- config/build/vite.config.ts
+++ config/build/vite.config.ts
@@ -1,28 +1,9 @@
-import { defineConfig, type Plugin } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-import ts from "rollup-plugin-ts"
-import { di } from "@wessberg/di-compiler"
-
-// https://github.com/wessberg/DI-compiler
-const diPlugin = (options = {}): Plugin => {
-  return {
-    ...ts({
-      ...options,
-      transformers: [di],
-      transpileOnly: true,
-      exclude: ["**/*.test.ts"],
-      tsconfig: resolvedConfig => ({
-        ...resolvedConfig,
-        declaration: false,
-      }),
-    }),
-    name: "vite:di",
-    enforce: "pre",
-  }
-}
+import { diPlugin } from "./diPlugin"
 
 export default defineConfig({
   plugins: [svgr(), react(), tsconfigPaths(), diPlugin()],
