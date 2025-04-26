a011746ba532077fc6df2a04d4d3ac38ac722bb2
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:22:41 +0300

    feat(types): add utility types
    types for extracting keys, values and types of instances for constructors combined in object

--- src/shared/types/utils.d.ts
+++ src/shared/types/utils.d.ts
@@ -1,3 +1,8 @@
 const __brand: unique symbol
 
 type Brand<T> = T & { readonly [__brand]: unique symbol }
+
+type Keys<T> = keyof T
+type Values<T> = T[Keys<T>]
+
+type InstanceValues<T> = InstanceType<Values<T>>
