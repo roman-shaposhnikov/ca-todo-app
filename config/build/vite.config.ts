a00571f867ea44717def0824be6a5cc17aa2ef72
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-04-26 19:22:41 +0300

    build(vite): change custom di plugin to vite-wessberg-di

--- config/build/vite.config.ts
+++ config/build/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
 import tsconfigPaths from "vite-tsconfig-paths"
-
-import { diPlugin } from "./diPlugin"
+import di from "vite-wessberg-di"
 
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths(), diPlugin()],
+  plugins: [svgr(), react(), tsconfigPaths(), di()],
 
   root: "src/app", // index.html directory
   envDir: "../../", // relative to "root"
